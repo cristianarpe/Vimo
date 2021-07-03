66c7c2124b85193aecd2cf7688c12354a612e531
juliangonzalez-ing <edgar.gonzalez123@outlook.com>
2021-07-03 13:11:24 -0500

    controlador admin

--- app/Http/Controllers/TbPeliculasController.php
+++ app/Http/Controllers/TbPeliculasController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\tb_peliculas;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class TbPeliculasController extends Controller
 {
