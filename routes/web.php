6fc40d9019994f6e1790092ca7d8f25c3ce88fea
Hector Duque <82244186+elmismo2083@users.noreply.github.com>
2021-07-11 16:02:59 -0500

    actualizacion index laravel

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome'); /* esto me lleva a welcome.blade.php */
+    return view('auth.login'); /* esto me lleva a welcome.blade.php */
 });
 
 /* Auth::routes();*/
